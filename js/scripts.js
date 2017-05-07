a696b95355f768dfc1d2d5d3e47f761bb0e132f7
Adrian <tommazost@gmail.com>
2017-05-07 09:31:00 +0200

    first exercise jquery

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var span = $('span');
+
+span.each(function(index, element) {
+    if (index %1==0) {
+        $(element).css('color', 'yellow')
+    };
+});
+
+var paragraphs = $('p');
+paragraphs.each(function(index, element) {
+
+        var button = '<button class="btn" data-tmp="'+index+'">Click me</button>'
+        $(element).append(button)
+});
+
+$('button').click(function(){
+    alert($(this).attr('data-tmp'));
+});
